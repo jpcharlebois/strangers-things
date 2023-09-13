2a6335bc238d476bb13ba769e1e3693f6d917bda
JP <jp@JPPC.localdomain>
2023-09-12 20:08:54 -0700

    added search post functionality

--- src/components/RenderPosts.jsx
+++ src/components/RenderPosts.jsx
@@ -6,11 +6,30 @@ import { fetchPosts, myData } from '../api/ajaxHelper';
 export default function RenderPosts({ token }) {
   const [posts, setPosts] = useState([]);
   const [user, setUser] = useState({});
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
+  
+  function postMatches(post, text) {
+    // return true if any of the fields you want to check against include the text
+    // strings have an .includes() method
+    if (post.title.includes(text)) {
+      return true;
+    } else if (post.description.includes(text)) {
+      return true;
+    }
+    return false;
+  }
+  
+  const filteredPosts = posts.filter((post) => postMatches(post, searchTerm));
+  const postsToDisplay = searchTerm.length ? filteredPosts : posts;
+
+  const handleChange = async (e) => {
+    setSearchTerm( e.target.value )
+  }
 
   function renderPosts() {
-    console.log("posts: ", posts);
-    return posts.map((post) => {
+    console.log("posts: ", postsToDisplay);
+    return postsToDisplay.map((post) => {
       return (
         <div key={post._id} className="post" >
           <h2>{post.title}</h2>
@@ -19,9 +38,9 @@ export default function RenderPosts({ token }) {
           <h4>{post.location}</h4>
           <p>{post.author.username}</p>
           {(token && post.author._id !== user._id) ?
-<button onClick={() => navigate(`/posts/${post._id}/message`)}>Send Message</button> :
-<button onClick={() => navigate(`/posts/${post._id}`)}>VIEW</button> 
-}
+            <button onClick={() => navigate(`/posts/${post._id}/message`)}>Send Message</button> :
+            <button onClick={() => navigate(`/posts/${post._id}`)}>VIEW</button> 
+          }
         </div>
       )
     })
@@ -48,7 +67,17 @@ export default function RenderPosts({ token }) {
     <div>
       <div>
         <h2>Posts</h2>
-        <Link to={`/posts/addNewPost`}>ADD POST</Link>
+        { (token) ?
+        <Link to={`/posts/addNewPost`}>ADD POST</Link> :
+        <p></p>
+        }
+      </div>
+      <div className='searchbar'>
+        <form>
+          <label>Search
+            <input onChange={handleChange} value={searchTerm} type="search"/>
+          </label>
+        </form>
       </div>
       <div>
         {renderPosts()}
